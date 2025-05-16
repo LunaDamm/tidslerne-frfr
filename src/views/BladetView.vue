ef9dca0f57e33af05597ea2001443fbcf50fb6ac
Matmil01 <matmil01@easv365.dk>
2025-05-16 17:41:59 +0200

    Update BladetView.vue

--- src/views/BladetView.vue
+++ src/views/BladetView.vue
@@ -18,39 +18,55 @@ console.log(bladet);
     <!-- Tekst -->
     <div class="relative z-10 container mx-auto px-4 text-center">
       <h1 class="text-light-green font-headline font-bold text-[64px] pt-75">
-        Bladet Tidslerne
+        Bladet 'Tidslerne'
       </h1>
     </div>
   </div>
 
   <div class="p-5"></div>
 
-   <!-- artikel = blad
-        artikler = bladet-->
+  <p class="text-dark-green text-[18px]">Nedenfor har du alle udgivne medlemsblade Tidslerne.</p>
+  <br></br>
+  <p class="text-dark-green text-[18px]">Du kan læse bladene direkte her fra siden. Klik på et blad for at åbne det.</p>
 
-    <div>
-    <div class="flex flex-wrap">
-          <!-- repeat blad thumbnail -->
-          <div class="flex flex-wrap w-full gap-4 justify-center">
-            <a :href="blad.pdf" class="w-3./12" v-for="blad in bladet" :key="blad.id">
-              <img :src="blad.thumbnail" alt="{{ blad.name }} thumbnail" class="h-70 w-full object-cover rounded-[5px]" />
-              <p class="text-2xl">{{ blad.name }}</p>
-              <p>{{ blad.description }}</p>
-            </a>
-          </div>
-        </div>
-  </div>
+<div class="p-5"></div>
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-6">
+  <a
+    v-for="blad in bladet"
+    :key="blad.id"
+    :href="blad.pdf"
+    class="block transform transition duration-300 hover:scale-[1.02] hover:shadow-lg rounded-lg"
+  >
+    <img
+      :src="blad.thumbnail"
+      :alt="blad.name + ' thumbnail'"
+      class="w-full object-contain rounded-[5px] max-h-[220px] md:max-h-[260px] lg:max-h-[350px] transition duration-300 hover:scale-105"
+    />
+    <p class="text-2xl mt-2 font-semibold text-center">{{ blad.name }}</p>
+    <p class="text-center">{{ blad.description }}</p>
+  </a>
+</div>
+
+
+
+
+
+
+
+
 
    <!-- BLAD SLUT HER -->
 
 
-  <h1 class="text-dark-green font-bold text-[48px]">Bladet</h1>
 
   <div class="p-5"></div>
 
+<div class="w-full flex justify-center mt-10">
   <button class="bg-button-green text-light-green px-6 py-3 rounded-[5px] text-lg">
     Se mere
   </button>
+</div>
 
 
   <div class="p-5"></div>

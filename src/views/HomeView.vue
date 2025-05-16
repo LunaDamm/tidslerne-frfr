710f722327b2c2aac0f260329904c331238dbb22
Emisim01 <emisim01@easv365.dk>
2025-05-16 09:42:14 +0200

    link ændret på knappper

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,19 @@
 
 <div class="heroImg p-70 w-4/4"></div>
 
+
+
 <section class="medlemOgFordele flex pb-16">
 <div class="Imgcontainer p-40 w-2/4 rounded-sm"></div>
 
 <div class="flex flex-col items-center justify-center pl-48 ">
 <p class="OverskriftButton w-72 text-dark-green pb-4">Halløjsa! Bliv medlem her og få en masse fordele</p>
 <div class="flex flex-col gap-4 w-full items-center font-bold">
-  <button class="buttonTekst bg-button-green text-light-green px-8 py-2 rounded-sm mb-4 w-72">Medlem</button>
-    <button class="buttonTekst bg-button-green text-light-green px-8 py-2 rounded-sm  w-72">Fordele</button>
+
+<a href="https://medlem.tidslerne.dk/" class="buttonTekst bg-button-green text-light-green px-8 py-2 rounded-sm mb-4 w-72 flex justify-center items-center">Medlem</a>
+
+<a href="https://medlem.tidslerne.dk/" class="buttonTekst bg-button-green text-light-green px-8 py-2 rounded-sm mb-4 w-72 flex justify-center items-center">Fordele</a>
+
 </div>
 </div>
 </section>

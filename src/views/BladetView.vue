3c2f872f5f72171faebbe7d07258103a93a535f4
Matmil01 <matmil01@easv365.dk>
2025-05-17 14:55:50 +0200

    media queeries på bladet hero

--- src/views/BladetView.vue
+++ src/views/BladetView.vue
@@ -9,23 +9,29 @@ console.log(bladet);
 </script>
 
 <template>
-  <div class="relative w-full h-[400px] md:h-[800px]">
-    <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('/images/midt-om-natten.png');"></div>
+<div class="relative w-full h-[400px] md:h-[800px]">
+  <div
+    class="absolute inset-0 bg-cover bg-center"
+    style="background-image: url('/images/midt-om-natten.png');"
+  ></div>
 
-    <!-- Overlay -->
-    <div class="absolute inset-0 bg-black opacity-50"></div>
+  <!-- Overlay -->
+  <div class="absolute inset-0 bg-black opacity-50"></div>
 
-    <!-- Tekst -->
-    <div class="relative z-10 container mx-auto px-4 text-center">
-      <h1 class="text-light-green font-headline font-bold text-[64px] pt-75">
-        Bladet 'Tidslerne'
-      </h1>
-    </div>
+  <!-- Text -->
+  <div class="relative z-10 container mx-auto px-4 text-center flex items-center justify-center h-full">
+    <h1
+      class="text-light-green font-headline font-bold text-[32px] sm:text-[40px] md:text-[48px] lg:text-[64px] leading-tight"
+    >
+      Bladet 'Tidslerne'
+    </h1>
   </div>
+</div>
+
 
   <div class="p-5"></div>
 
-  <p class="text-dark-green text-[18px]">Nedenfor har du alle udgivne medlemsblade Tidslerne.</p>
+  <p class="text-dark-green text-[18px]">Nedenfor har du alle udgivne medlemsblade fra Tidslerne.</p>
   <br></br>
   <p class="text-dark-green text-[18px]">Du kan læse bladene direkte her fra siden. Klik på et blad for at åbne det.</p>
 

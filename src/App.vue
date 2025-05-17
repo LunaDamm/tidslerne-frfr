76c6ff64a0dd898d29b65363f39ec945c8c8996f
Emisim01 <emisim01@easv365.dk>
2025-05-17 16:05:35 +0200

    facebook og insta til footer

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
   <footer>
     <div class="w-4/4 bg-nav-green">
-<div class="footerBigBox text-light-green flex flex-col md:flex-row items-center">
+<div class="footerBigBox text-light-green flex flex-row justify-between">
 
     <div class="footerBox1 ">
         <RouterLink class="footer-link" to="#">Kontakt os</RouterLink>
@@ -74,6 +74,20 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink class="footer-link" to="#">Merch</RouterLink>
         <a class="footer-link" href="https://medlem.tidslerne.dk/">Login</a>
         <a class="footer-link" href="https://medlem.tidslerne.dk/">Bliv medlem</a>
+      </div>
+
+      <div class="flex items-center gap-4 mt-6 ml-auto">
+      <div class="facebookLogo ">
+        <a href="https://www.facebook.com/groups/1480251482282861/?ref=group_header">
+          <img src="@/assets/facebook.png" alt="Facebook" class="w-10 h-10" />
+        </a>
+        </div>
+
+        <div class="instagramLogo">
+            <a href="https://instagram.com">
+            <img src="@/assets/instagram.png" alt="Instagram" class="w-10 h-10" />
+             </a>
+           </div>
       </div>
 
     </div>
@@ -151,12 +165,18 @@ font-size: 20px;
 padding: 33px 0 33px 117px;
 }
 
-.footerBox1 {
-margin-right: 189px;
+.footerBox2 {
+margin-right: 400px ;}
+
+
+.facebookLogo{
+  border-right: solid 2px var(--color-light-green);
+  padding-right: 15px;
 }
 
-.footerBox2 {
-margin-right: 197px;
+
+.instagramLogo{
+  margin-right: 117px;
 }
 
 

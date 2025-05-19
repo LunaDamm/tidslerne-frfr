4c8c6a1d3a96409c559c6a98f06b1bf1f5204b2c
Mirkel1998 <mikped01@easv365.dk>
2025-05-19 10:29:37 +0200

    all pictures added to medlemsfordele
    they dont fit proberly yet

--- src/views/MedlemsfordeleView.vue
+++ src/views/MedlemsfordeleView.vue
@@ -18,21 +18,21 @@
   <div class="flex flex-col md:flex-row justify-evenly items-start gap-y-10 gap-x-6">
     <!-- Company 1 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/Mai-Nielsen3.png" alt="Mai Nielsen" class="w-full h-auto rounded-[5px]"/>
+      <img src="/images/pharmanordLogo.webp" alt="Pharma Nord logo" class="w-full h-auto rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Pharma Nord</p>
       <p class="text-dark-green font-bold text-[20px]">38% rabat</p>
          </div>
 
     <!-- Company 2 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/nynne_afkjaer.png" alt="Nynne Afkjær" class="w-full h-auto rounded-[5px]"/>
+      <img src="/images/naturDrogerietLogo.png" alt="Natur Drogeriet logo" class="w-full h-auto rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Natur Drogeriet</p>
       <p class="text-dark-green font-bold text-[20px]">30% rabat</p>
     </div>
 
     <!-- Company 3 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/hovedskou.png" alt="Christina Hovedskou" class="w-full h-auto rounded-[5px]"/>
+      <img src="/images/nyborggaardLogo.webp" alt="Nyborggaard logo" class="w-full h-auto rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Nyborggaard</p>
       <p class="text-dark-green font-bold text-[20px]">20% rabat</p>
     </div>
@@ -43,21 +43,21 @@
   <div class="flex flex-col md:flex-row justify-evenly items-start gap-y-10 gap-x-6">
     <!-- Company 4 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/dorthehelbrink.jpg" alt="Dorthe Helbrink" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
+      <img src="/images/bioforceLogo.png" alt="Bioforce logo" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Bioforce</p>
       <p class="text-dark-green font-bold text-[20px]">20% rabat</p>
     </div>
 
     <!-- Company 5 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/dorthe_olsen.jpg" alt="Dorthe Vinther Olsen" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
+      <img src="/images/goldenHealtLogo.png" alt="Golden Health logo" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Golden Health</p>
       <p class="text-dark-green font-bold text-[20px]">20% rabat</p>
     </div>
 
     <!-- Company 6 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/Heidi-Hansen.jpg" alt="Heidi Hansen" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
+      <img src="/images/khosmosLogo.jpeg" alt="Khosmos logo" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Khosmos</p>
       <p class="text-dark-green font-bold text-[20px]">15% rabat</p>
     </div>
@@ -69,7 +69,7 @@
   <div class="flex flex-col md:flex-row justify-center items-start gap-y-10 gap-x-6">
     <!-- Company 7 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/Marianne-Brodsgaard.jpg" alt="Marianne Brødsgaard" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
+      <img src="/images/zoocaLogo.png" alt="Zooca logo" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Zooca</p>
       <p class="text-dark-green font-bold text-[20px]">15% rabat</p>
     </div>
@@ -78,7 +78,7 @@
   <div class="flex flex-col md:flex-row justify-center items-start gap-y-10 gap-x-6">
     <!-- Company 8 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/Marianne-Brodsgaard.jpg" alt="Marianne Brødsgaard" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
+      <img src="/images/drizzleLogo.png" alt="Drizzle Life logo" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Drizzle life</p>
       <p class="text-dark-green font-bold text-[20px]">10% rabat</p>
     </div>
@@ -87,7 +87,7 @@
   <div class="flex flex-col md:flex-row justify-center items-start gap-y-10 gap-x-6">
     <!-- Company 9 -->
     <div class="w-[15.5rem] text-left mx-auto">
-      <img src="/images/Marianne-Brodsgaard.jpg" alt="Marianne Brødsgaard" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
+      <img src="/images/sundtVandLogo.png" alt="Sundt Vand logonpm" class="w-[248px] h-[310px] object-cover rounded-[5px]"/>
       <p class="mt-2 text-dark-green font-bold text-[25px]">Sundt-Vand</p>
       <p class="text-dark-green font-bold text-[20px]">10% rabat</p>
     </div>
@@ -96,6 +96,8 @@
 
   <div class="p-5"></div>
 </div>
+
+
   </template>
 
 
